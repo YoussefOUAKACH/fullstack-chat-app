9938cd0413639f57da705f969c7cec56a81e7fbf
youssef OUAKACH <57898027+YoussefOUAKACH@users.noreply.github.com>
2025-07-10 20:43:56 +0000

    Pending changes exported from your codespace

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import fs from "fs";
 
 import path from "path";
 
-import { connectDB } from "./lib/db.js";
+import connectDB from "./lib/db.js";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
+console.log("ENV file exists:", fs.existsSync("./.env"));
+dotenv.config({ path: "./.env" });
 
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
